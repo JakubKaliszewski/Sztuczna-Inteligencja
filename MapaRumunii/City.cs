36cecb339b6cef8aa4c04858c9fb7d1816ace3cd
JakubKaliszewski <kontakt@jakubkaliszewski.pl>
2018-01-02 15:20:21 +0100

    Dodano klasy City orz Neighbor

---
+++ MapaRumunii/City.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace MapaRumunii
+{
+    public class City
+    {
+        private static int number;
+        private int id { get;  }
+        private string name { get; } 
+        private List<Neighbor> neighborsCities { get; }
+
+        public City(string name)
+        {
+            id = number;
+            this.name = name;
+            neighborsCities = new List<Neighbor>();
+            number++;
+        }
+
+        public void AddNeighbor(City cityToAdd, int distance)
+        {
+            Neighbor neighbor = new Neighbor(distance,cityToAdd);
+            neighborsCities.Add(neighbor);
+        }
+    }
+}
---
+++ MapaRumunii/Neighbor.cs
@@ -0,0 +1,17 @@
+﻿namespace MapaRumunii
+{
+    public class Neighbor
+    {
+        private int distance;
+        private City city;
+
+        public Neighbor(int distance, City city)
+        {
+            this.distance = distance;
+            this.city = city;
+        }
+        
+        
+    }
+   
+}
