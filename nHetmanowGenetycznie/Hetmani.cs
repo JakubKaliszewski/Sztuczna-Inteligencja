1ed8ef277b45495f6120709cbab4721811da1e8d
JakubKaliszewski <kontakt@jakubkaliszewski.pl>
2018-01-20 23:15:49 +0100

    Algorytm Generyczny - Tworzenie Losowej Populacji

--- nHetmanowGenetycznie/Hetmani.cs
+++ nHetmanowGenetycznie/Hetmani.cs
@@ -1,48 +1,58 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace nHetmanowGenetycznie
 {
-    public class Hetmani : AlgorytmGenetyczny<byte>
+    public class Hetmani : AlgorytmGenetyczny<byte[]>
     {
-        protected override byte[] LosowaPopulacja(int rozmiar)
+        public Hetmani(int rozmiar)
         {
-            byte[] zwracanaPopulacja = {0, 1, 2, 3, 4, 5, 6, 7};
+            RozmiarPopulacji = rozmiar;
+        }
+        
+        protected override byte[][] LosowaPopulacja(int rozmiar)
+        {
+            byte[][] zwracanaPopulacja = new byte[rozmiar][];
+            byte[] przykladowyOsobnik = {0, 1, 2, 3, 4, 5, 6, 7};
             Random losowa = new Random();
             const int iloscLosowan = 30;
 
-
-            for (int i = 0; i < iloscLosowan; i++)
+            for (int osobnik = 0; osobnik < rozmiar; osobnik++)
             {
-                byte tymczasowaDoZamiany;
-                int wylosowana1, wylosowana2;
+                for (int i = 0; i < iloscLosowan; i++)
+                {
+                    byte tymczasowaDoZamiany;
+                    int wylosowana1, wylosowana2;
 
-                wylosowana1 = losowa.Next(1, 8);
-                wylosowana2 = losowa.Next(1, 8);
+                    wylosowana1 = losowa.Next(1, 8);
+                    wylosowana2 = losowa.Next(1, 8);
 
-                tymczasowaDoZamiany = zwracanaPopulacja[wylosowana1];
-                zwracanaPopulacja[wylosowana1] = zwracanaPopulacja[wylosowana2];
-                zwracanaPopulacja[wylosowana2] = tymczasowaDoZamiany;
+                    tymczasowaDoZamiany = przykladowyOsobnik[wylosowana1];
+                    przykladowyOsobnik[wylosowana1] = przykladowyOsobnik[wylosowana2];
+                    przykladowyOsobnik[wylosowana2] = tymczasowaDoZamiany;
+                    zwracanaPopulacja[osobnik] = przykladowyOsobnik;
+                }
             }
-
+        
             return zwracanaPopulacja;
         }
 
-        protected override byte Koniec(bool bestPossible = false)
+        protected override byte[] Koniec(bool bestPossible = false)
         {
             throw new NotImplementedException();
         }
 
-        protected override float Przystosowanie(byte osobnik)
+        protected override float Przystosowanie(byte[] osobnik)
         {
             throw new NotImplementedException();
         }
 
-        protected override void Krzyzuj(byte osobnik1, byte osobnik2, out byte nowyOsobnik1, out byte nowyOsobnik2)
+        protected override void Krzyzuj(byte[] osobnik1, byte[] osobnik2, out byte[] nowyOsobnik1, out byte[] nowyOsobnik2)
         {
             throw new NotImplementedException();
         }
 
-        protected override byte Mutacja(byte osobnik)
+        protected override byte[] Mutacja(byte[] osobnik)
         {
             throw new NotImplementedException();
         }
--- nHetmanowGenetycznie/Program.cs
+++ nHetmanowGenetycznie/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace nHetmanowGenetycznie
+﻿using System;
+
+namespace nHetmanowGenetycznie
 {
     internal class Program
     {
-        public static void Main(string[] args)
-        {
+        public static void Main()
+        {           
+            Console.WriteLine("Podaj liczbę iteracji algorytmu genetycznego: ");
+            int liczbaIteracji = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Podaj rozmiar populacji: ");
+            int rozmiarPopulacji = Convert.ToInt32(Console.ReadLine());
+            
+            Hetmani problemHetmani = new Hetmani(rozmiarPopulacji);
+            problemHetmani.Szukaj(liczbaIteracji);
         }
     }
 }
